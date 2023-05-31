4398cd615fc8b5a2c2588fd5cc157f42123cbfb6
fuglyuckDev <harrisoncwalton@gmail.com>
2023-05-31 03:23:11 +0100

    Update Hero.jsx
    MP4 conversion for dirty iPhone users

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = ({ title }) => {
     <div className={styles.heroContainer}>
       <HeroTitle text={title} />
       <video autoPlay muted loop className={styles.video}>
-        <source src="/videos/louby_lou_promo.webm" type="video/webm" />
+        {/* <source src="/videos/louby_lou_promo.webm" type="video/webm" /> */}
         <source src="/videos/louby_lou_promo.mp4" type="video/mp4" />
       </video>
     </div>
