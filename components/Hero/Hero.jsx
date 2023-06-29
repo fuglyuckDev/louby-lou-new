b6593d041f728e212ea4248c4e228c81b3be25b4
fuglyuckDev <harrisoncwalton@gmail.com>
2023-06-29 19:38:11 +0100

    More optimization
    Added poster and mp4 alternative for ios, for some reason safari still doesn't support webm

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -6,8 +6,16 @@ const Hero = ({ title, button, src }) => {
   return (
     <div className={styles.heroContainer}>
       <HeroTitle text={title} button={button} />
-      <video autoPlay muted loop playsInline className={styles.video}>
-        <source src={src} type="video/webm" />
+      <video
+        poster="/images/pictures/louby_lou_hero_poster.webp"
+        autoPlay
+        muted
+        loop
+        playsInline
+        className={styles.video}
+      >
+        <source src={`${src}.webm`} type="video/webm" />
+        <source src={`${src}.mp4`} type="video/mp4" />
       </video>
     </div>
   );
